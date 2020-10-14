299ca871ab109ef08a648b4d87073580a3fe8b4c
Vladislav <vladby86@gmail.com>
2020-10-14 20:45:44 +0300

    Updates

---
+++ precache-manifest.782a116a8f2398bf90ae953f90115252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f03c9243011173fb34b34734032729c",
+    "url": "/Portfolio_ts/index.html"
+  },
+  {
+    "revision": "e98f1ef69b28f70f011d",
+    "url": "/Portfolio_ts/static/css/main.b0fedef7.chunk.css"
+  },
+  {
+    "revision": "a51a8827f8abd64aa0aa",
+    "url": "/Portfolio_ts/static/js/2.30126f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio_ts/static/js/2.30126f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98f1ef69b28f70f011d",
+    "url": "/Portfolio_ts/static/js/main.5bbd93dd.chunk.js"
+  },
+  {
+    "revision": "eb84bf3bf66588ef3561",
+    "url": "/Portfolio_ts/static/js/runtime-main.68f2c0b6.js"
+  }
+]);

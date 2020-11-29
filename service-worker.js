f7ddd9517b3aed115bb2c5efb839a36082a10fd5
Vladislav <vladby86@gmail.com>
2020-11-30 02:15:40 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio_ts/precache-manifest.4b2bc21b0d38118710b81dfbabb70ef9.js"
+  "/Portfolio_ts/precache-manifest.b2924134d5ebecb3bc9fce72268ba37a.js"
 );
 
 self.addEventListener('message', (event) => {
